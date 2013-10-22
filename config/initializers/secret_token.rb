8f8347a58220d03632534f6d5b376bdecfbdf387
Dominik Fischer <dfischer.sr@gmail.com>
2013-10-22 09:49:07 -0300

    ruby installed

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tasking::Application.config.secret_key_base = 'c3ac91859a61521321f9e9f5ef011045b96b9b492382436111a8357e9135b37e8884d964ad8c9e16692deb7e3f0bc7fc3b06bc4aa7c0b2d6d7407dd2f0451c9c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Tasking::Application.config.session_store :cookie_store, key: '_tasking_session'
