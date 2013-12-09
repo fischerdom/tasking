b681af293f63a659a9d2675aee6d08a3fb85e154
alfransmic <michael.alfranseder@web.de>
2013-12-09 17:49:14 +0100

    Revert "Heroku bugfix"
    This reverts commit 43c059feb0a39f74be2ae385f342aa572fefd59d.

--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,4 @@ module Tasking
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
   end
-  config.assets.precompile << '*.js'
-  
 end
