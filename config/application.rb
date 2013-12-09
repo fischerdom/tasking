43c059feb0a39f74be2ae385f342aa572fefd59d
alfransmic <michael.alfranseder@web.de>
2013-12-09 17:33:59 +0100

    Heroku bugfix

--- config/application.rb
+++ config/application.rb
@@ -20,4 +20,6 @@ module Tasking
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
   end
+  config.assets.precompile << '*.js'
+  
 end
