ae15b12bd1d42a788ab4fba19d8a32b0331e5750
alfransmic <michael.alfranseder@web.de>
2013-12-19 14:20:50 +0100

    SSL-Error umgehen

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -6,6 +6,7 @@ Tasking::Application.configure do
   # Code is not reloaded between requests.
   config.cache_classes = true
 
+  OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both thread web servers
   # and those relying on copy on write to perform better.
